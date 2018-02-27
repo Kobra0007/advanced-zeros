255176a2b4f46d43c5d8c4fbd087a7555db6057c
Kobra0007 <kobra007@github.com>
2018-02-28 02:12:36 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+    let dividers = findDivider(base);
+    let powsOfDev = dividers.reduce((acc, el) => {
+        acc[el] = (acc[el] || 0) + 1;
+        return acc;
+    }, {});
+
+    for (let divider in powsOfDev) {
+        powsOfDev[divider] = {
+            divider: +divider,
+            pow: powsOfDev[divider],
+            summ: 0,
+        }
+    }
+
+    for (let value in powsOfDev) {
+        for (let i = powsOfDev[value].divider; number / i >= 1; i = i * powsOfDev[value].divider) {
+            powsOfDev[value].summ += Math.floor(number / i);
+        }
+    }
+
+    let zeros = [];
+    for (let value in powsOfDev) {
+        zeros.push(Math.floor(powsOfDev[value].summ / powsOfDev[value].pow));
+    }
+    return Math.min(...zeros);
+
+};
+
+function findDivider(base) {
+    let dividerArr = [];
+    for (let i = 2; i <= base; i++) {
+        while (base % i == 0) {
+            dividerArr.push(i);
+            base /= i;
+        }
+    }
+    return dividerArr;
 }
